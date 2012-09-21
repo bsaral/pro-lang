ee533fbdc3773655569c4a8a42b3b2a2ae47ec97
bsaral <betul.saral@bil.omu.edu.tr>
2012-09-21 21:52:27 +0300

    .

--- Python/PROJECT EULER/project euler-1.py
+++ Python/PROJECT EULER/project euler-1.py
@@ -1,20 +1,20 @@
-# -*- coding: cp1254 -*-
-# PROJECT EULER 1
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 ##################################################################################################
+# PROJECT EULER 1                                                                                #
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.#
 # The sum of these multiples is 23.                                                              #
 # Find the sum of all the multiples of 3 or 5 below 1000.                                        #   
 #                                                                                                #
-# 1000 altýnda olan 3 ve 5 bölünebilen doðal sayýlar toplamý ?                                #
+# 1000 altÄ±nda olan 3 ve 5 bÃ¶lÃ¼nebilen doÄŸal sayÄ±lar toplamÄ± ?                                   #
 #                                                                                                #
 ##################################################################################################
 
-numbers = range(1,1000,1)
-print sum (filter ( lambda x:x%3==0 or x%5==0,numbers))
+numbers = range(1, 1000, 1)
+print sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, numbers))
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()    
 
-    
-
-
-    

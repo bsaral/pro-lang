132bf635fee02cd36a94802d761d9678bf80d6a9
bsaral <betul.saral@bil.omu.edu.tr>
2013-03-11 00:04:51 +0200

    java

---
+++ Java/First_example/src/Merhaba.java
@@ -0,0 +1,11 @@
+
+public class Merhaba {
+	
+	public static void main(String[] args)  {
+		String isim = "BETÜL";
+		String soyisim = "SARAL";
+		
+		System.out.println("Merhaba"+" "+isim+" "+soyisim);
+		
+	}
+}

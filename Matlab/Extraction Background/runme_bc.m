ef1795f17c6a22dd616f807f6b0b66675bbad7ae
Betül SARAL <betul.saral@bil.omu.edu.tr>
2013-10-11 22:06:42 +0300

    güncelleme yapıldı

--- Matlab/Extraction Background/runme_bc.m
+++ Matlab/Extraction Background/runme_bc.m
@@ -6,20 +6,9 @@ im3 = imsubtract(im1,im2);
 im3 = bwareaopen(im3,500,8);
 im3 = imfill(im3,'holes');
 im3=uint8(im3);
-[r c]=size(im3);
-
-for i=1:r
-    for j=1:c
-        if im3(i,j)==255
-            im3(i,j)=1;
-        
-        end
-    end
-end
-
-im(:,:,1)=input(:,:,1).*im3;
-im(:,:,2)=input(:,:,2).*im3;
-im(:,:,3)=input(:,:,3).*im3;
+im3(im3 == 255) = 1;
+im33 = cat(3, im3, im3, im3); 
+im = input .* im33; 
 imshow(im);
 
 
